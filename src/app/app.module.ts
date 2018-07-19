1c2f1d0bc892879c32c66bf815bc65c5052363b5
Unknown <javrm357@gmail.com>
2018-07-19 13:35:20 +0200

    ADD FormsModule + HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routingProvider } from './routing';
 
 import { AppComponent } from './app.component';
@@ -15,6 +17,8 @@ import { PageNotFoundComponent } from './commons/page-not-found/page-not-found.c
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     routingProvider
   ],
