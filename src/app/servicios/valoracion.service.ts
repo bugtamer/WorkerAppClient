ddf9593092bc3b405ccf582354554d4173dd10ee
Unknown <javrm357@gmail.com>
2018-07-20 19:27:29 +0200

    Componente detalle: recuperación de la valoración media

---
+++ src/app/servicios/valoracion.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators'
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValoracionService {
+
+  private _valoracionStore:number;
+  private _valoracionObs:Observable<number>;
+
+  constructor(private _httpClient:HttpClient) { }
+
+
+
+  getValoracionFromApi(valId:number):Observable<number> {
+    if (this._valoracionStore) {
+      this._valoracionObs = of(this._valoracionStore);
+    } else if (this._valoracionObs) {
+      // observable ya esta en curso
+    } else {
+      this._valoracionObs = this._httpClient.get<number>(
+        `http://localhost:8080/WorkerApp2/api/profesional/${valId}/valoracion/media`
+      ).pipe(
+        tap(
+          data => {
+            console.log("data", data);
+            this._valoracionStore = data;
+            localStorage.setItem('valoracion', JSON.stringify(this._valoracionStore));
+          },
+          error => console.log('ValoracionService', error)
+        ));
+    }
+    return this._valoracionObs;
+  }
+
+
+
+}
