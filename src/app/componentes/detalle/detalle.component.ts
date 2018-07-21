14ee16f7953c8ba5c2d031dea31959e702ff2dc3
bugtamer <javrm357@gmail.com>
2018-07-21 08:57:01 +0200

    FIX merging errors

--- src/app/componentes/detalle/detalle.component.ts
+++ src/app/componentes/detalle/detalle.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ValoracionService } from '../../servicios/valoracion.service';
-<<<<<<< HEAD
 import { ProfesionalService } from '../../servicios/profesional.service';
 import { Profesional } from '../../modelos/Profesional';
-=======
->>>>>>> master
+
 
 @Component({
   selector: 'app-detalle',
@@ -16,38 +14,40 @@ export class DetalleComponent implements OnInit {
 
   _pid:number;
   _valoracionMedia:number;
-<<<<<<< HEAD
   _profesional:Profesional;
 
-  constructor(private _valoracionService:ValoracionService, private _profesionalService:ProfesionalService, private _route:ActivatedRoute) { }
-=======
+  
+  constructor(
+    private _valoracionService:ValoracionService,
+    private _profesionalService:ProfesionalService,
+    private _route:ActivatedRoute) { }
 
-  constructor(private _valoracionService:ValoracionService, private _route:ActivatedRoute) { }
->>>>>>> master
 
-  ngOnInit() {
-    this._route.params.subscribe( receivedPathParams => {
-    this._pid = receivedPathParams['pid'];
-<<<<<<< HEAD
+    ngOnInit() {
+      this._route.params.subscribe( receivedPathParams => {
+        this._pid = receivedPathParams['pid'];
+        this.setValoracionMedia(this._pid);
+        this.setProfesional(this._pid);
+      }
+    }
+
     
-    this._valoracionService.getValoracionFromApi(this._pid).subscribe( receivedValoracion => {
-      console.log('receivedValoracion', receivedValoracion);
-      this._valoracionMedia = receivedValoracion;
-      console.log('this._valoracionMedia', this._valoracionMedia);
+    setValoracionMedia(pid:number) {
+      this._valoracionService.getValoracionFromApi(this._pid).subscribe( receivedValoracion => {
+        console.log('receivedValoracion', receivedValoracion);
+        this._valoracionMedia = receivedValoracion;
+        console.log('this._valoracionMedia', this._valoracionMedia);
+      }
     }
+
     
-    this._profesionalService.getProfesionalFromApi(this._pid).subscribe( receivedProfesional => {
-      console.log('receivedProfesional', receivedProfesional);
-      this._profesional = receivedProfesional;
-      console.log('this._profesional', this._profesional);
+    setProfesional(pid:number) {
+      let pro:Profesional;
+      this._profesionalService.getProfesionalFromApi(this._pid).subscribe( receivedData => {
+        console.log('receivedProfesional', receivedData);
+        this._profesional = receivedData;
+        console.log('this._profesional', this._profesional);
+      }
     }
-=======
-    this._valoracionService.getValoracionFromApi(this._pid).subscribe( receivedValoracion => {
-      console.log('receivedTasks', receivedValoracion);
-      this._valoracionMedia = receivedValoracion;
-      console.log('this._valoracionMedia', this._valoracionMedia);
-    }
->>>>>>> master
-  }
 
 }
